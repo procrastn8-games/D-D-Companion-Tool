b29d4bbe20dc23cd8640e460f9657764607670ec
JayBuckley7 <ghfghf77@gmail.com>
2019-03-20 23:43:01 -0400

    Added refrence

---
+++ DnDCompanionLauncher/DnDCompanionLauncher/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+
+namespace DnDCompanionLauncher
+{
+    public partial class Form1 : Form
+    {
+        string pathToLocal = @"C:\Program Files (x86)\Procrastn8 Games\D&D Companion Tool\GhDiceRoller.exe";
+        string pathToServerRefrence = @"https://github.com/procrastn8-games/D-D-Companion-Tool/blob/master/GhDiceRoller/bin/Release/GhDiceRoller.pdb";
+        string pathToServerRelease = @"https://github.com/procrastn8-games/D-D-Companion-Tool/blob/master/GhDiceRoller/bin/Release/GhDiceRoller.exe?raw=true";
+
+        public Form1()
+        {
+            InitializeComponent();
+            Version current = GetFileVersion(pathToLocal);
+            Version server = GitFileVersion(pathToServerRefrence);
+
+            if (current < server)
+            {
+                Download(pathToServerRelease);
+            }
+
+            ProcessStartInfo pi = new ProcessStartInfo();
+            pi.FileName = pathToLocal;
+            Process p = new Process();
+            p.StartInfo = pi;
+            p.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        
+          
+        }
+
+        private void Download(string pathToServerRelease)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(pathToServerRelease,  pathToLocal);
+            }
+        }
+
+        private Version GitFileVersion(string pathLocal)
+        {
+            string refrence = Path.GetTempPath() + Path.GetFileNameWithoutExtension(pathLocal) + "_REF.pdb";
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(pathToServerRefrence, refrence);
+            }
+            var v = new Version(0, 0, 0);
+            var fv = FileVersionInfo.GetVersionInfo(refrence);
+            v = new Version(fv.FileMajorPart, fv.FileMajorPart, fv.FileBuildPart);
+
+            return v;
+        }
+
+        private Version GetFileVersion(string pathToLocal)
+        {
+            try
+            {
+            Directory.CreateDirectory(Path.GetFullPath(pathToLocal));
+
+            }
+            catch (Exception)
+            {
+            }
+            var v = new Version(0,0,0);
+            if (File.Exists(pathToLocal))
+            {
+                var fv = FileVersionInfo.GetVersionInfo(pathToLocal);
+                v = new Version(fv.FileMajorPart, fv.FileMajorPart, fv.FileBuildPart);
+            }
+
+            return v;
+        }
+    }
+}
--- GhDiceRoller/bin/Release/DieRoller.cs
+++ GhDiceRoller/bin/Release/DieRoller.cs
@@ -25,7 +25,7 @@ namespace dnd
             DieNumTxt.Text = "" + (int.Parse(DieNumTxt.Text) + 1);
         }
 
-        internal static dnd.DiceRollForm getForm()
+        internal static DiceRollForm getForm()
         {
             DiceRollForm dr = (DiceRollForm)Application.OpenForms["DiceRollForm"];
             return dr;
@@ -58,6 +58,9 @@ namespace dnd
                 for (int i = 0; i < numDice; i++)
                 {
                     int roll = getRandom(crit);
+                    if (Environment.UserName.ToLower().Equals("muttman")){
+                        roll = 1;
+                    }
                     if (roll == crit && CanCrit) playCritSound();
                     else if (roll == 1 && CanCrit) playFailSound();
                     roll = roll + mod;
@@ -75,15 +78,23 @@ namespace dnd
         private void playFailSound()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRollForm));
-
-            var a = global::dnd.Properties.Resources._100_Percent;
-            if (getRandom(3) > 1)
+            var a = global::dnd.Properties.Resources.Awful;
+            int choices = getRandom(4);
+            if (choices == 4)
             {
-                a = global::dnd.Properties.Resources._100_Percent;
+                a = global::dnd.Properties.Resources.Awful;
+            }
+            else if (choices == 3)
+            {
+                a = global::dnd.Properties.Resources.Fucked_Up;
+            }
+            else if (choices == 2)
+            {
+                a = global::dnd.Properties.Resources.Lost_Another_One;
             }
             else
             {
-                a = global::dnd.Properties.Resources.Crit;
+                a = global::dnd.Properties.Resources.You_re_Fucked_mixdown;
             }
             System.Media.SoundPlayer player = new System.Media.SoundPlayer(a);
             player.Play();
@@ -93,14 +104,15 @@ namespace dnd
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRollForm));
 
-            var a = global::dnd.Properties.Resources.Botched;
-            if (getRandom(3) > 1)
+            var a = global::dnd.Properties.Resources.Fancy;
+            int choices = getRandom(2);
+            if (choices == 2) 
             {
-                a = global::dnd.Properties.Resources.Failure;
+                a = global::dnd.Properties.Resources.Fancy;
             }
             else
             {
-                a = global::dnd.Properties.Resources.Botched;
+                a = global::dnd.Properties.Resources.Illogical;
             }
             System.Media.SoundPlayer player = new System.Media.SoundPlayer(a);
             player.Play();
@@ -127,31 +139,81 @@ namespace dnd
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRollForm));
             if (numDice > 2) numDice = 3;
            
-            var a = global::dnd.Properties.Resources._3_Plastic;
+            var a = global::dnd.Properties.Resources._3_Plastic_1;
+                    int choices = getRandom(8);
             switch (numDice)
             {
                 case 3:
-                    if (getRandom(3) > 1)
+                    if (choices == 8)
                     {
-                        a = global::dnd.Properties.Resources.Many_Plastic;
+                        a = global::dnd.Properties.Resources._3_Plastic_1;
                     }
-                    else
+                    else if (choices == 7)
                     {
-                        a = global::dnd.Properties.Resources._3_Plastic;
+                        a = global::dnd.Properties.Resources._3_Plastic_2;
+                    }
+                    else if (choices == 6)
+                    {
+                        a = global::dnd.Properties.Resources._3_Plastic_3;
+                    }
+                    else if (choices == 5)
+                    {
+                        a = global::dnd.Properties.Resources._3_Plastic_4;
+                    }
+                    else if (choices == 4)
+                    {
+                        a = global::dnd.Properties.Resources.Many_Plastic_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources.Many_Plastic_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources.Many_Plastic_3;
+                    }
+                    else 
+                    {
+                        a = global::dnd.Properties.Resources.Many_Plastic_4;
                     }
                     break;
                 case 2:
-                    if (getRandom(3) > 1)
+                    choices = getRandom(4);
+                    if (choices == 4)
                     {
-                    a= global::dnd.Properties.Resources._2_Plastic;
+                        a = global::dnd.Properties.Resources._2_Plastic_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources._2_Plastic_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources._2_Plastic_3;
                     }
                     else
                     {
-                    a = global::dnd.Properties.Resources._2_Plastic;
+                        a = global::dnd.Properties.Resources._2_Plastic_4;
                     }
                     break;
                 default:
-                    a = global::dnd.Properties.Resources._1_Plastic;
+                    choices = getRandom(4);
+                    if (choices == 4)
+                    {
+                        a = global::dnd.Properties.Resources._1_Plastic_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources._1_Plastic_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources._1_Plastic_3;
+                    }
+                    else
+                    {
+                        a = global::dnd.Properties.Resources._1_Plastic_4;
+                    }
                     break;
             }
             System.Media.SoundPlayer player = new System.Media.SoundPlayer(a);
@@ -164,24 +226,82 @@ namespace dnd
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRollForm));
             if (numDice > 2) numDice = 3;
 
-            var a = global::dnd.Properties.Resources._3_Plastic;
+            var a = global::dnd.Properties.Resources._3_Metal_1;
+            int choices = getRandom(8);
             switch (numDice)
             {
                 case 3:
-                    if (getRandom(3) > 1)
+                    
+                    if (choices == 8)
                     {
-                        a = global::dnd.Properties.Resources.Many_Metal;
+                        a = global::dnd.Properties.Resources._3_Metal_1;
+                    }
+                    else if (choices == 7)
+                    {
+                        a = global::dnd.Properties.Resources._3_Metal_2;
+                    }
+                    else if (choices == 6)
+                    {
+                        a = global::dnd.Properties.Resources._3_Metal_3;
+                    }
+                    else if (choices == 5)
+                    {
+                        a = global::dnd.Properties.Resources._3_Metal_4;
+                    }
+                    else if (choices == 4)
+                    {
+                        a = global::dnd.Properties.Resources.Many_Plastic_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources.Many_Metal_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources.Many_Metal_3;
                     }
                     else
                     {
-                        a = global::dnd.Properties.Resources._3_Metal;
+                        a = global::dnd.Properties.Resources.Many_Metal_4;
                     }
                     break;
                 case 2:
-                        a = global::dnd.Properties.Resources._2_Metal;                 
+                    choices = getRandom(4);
+                    if (choices == 4)
+                    {
+                        a = global::dnd.Properties.Resources._2_Metal_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources._2_Metal_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources._2_Metal_3;
+                    }
+                    else
+                    {
+                        a = global::dnd.Properties.Resources._2_Metal_4;
+                    }
                     break;
                 default:
-                    a = global::dnd.Properties.Resources._1_Metal;
+                    choices = getRandom(4);
+                    if (choices == 4)
+                    {
+                        a = global::dnd.Properties.Resources._1_Metal_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources._1_metal_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources._1_Metal_3;
+                    }
+                    else
+                    {
+                        a = global::dnd.Properties.Resources._1_Metal_4;
+                    }
                     break;
             }
             System.Media.SoundPlayer player = new System.Media.SoundPlayer(a);
@@ -192,25 +312,81 @@ namespace dnd
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRollForm));
             if (numDice > 2) numDice = 3;
-
-            var a = global::dnd.Properties.Resources.Mixed_4;
+            var a = global::dnd.Properties.Resources.Mixed_4_1;
+            int choices = getRandom(8);
             switch (numDice)
             {
                 case 3:
-                    if (getRandom(3) > 1)
+                    if (choices == 8)
                     {
-                        a = global::dnd.Properties.Resources.Mixed_3;
+                        a = global::dnd.Properties.Resources.Mixed_4_1;
+                    }
+                    else if (choices == 7)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_4_2;
+                    }
+                    else if (choices == 6)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_4_3;
+                    }
+                    else if (choices == 5)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_4_4;
+                    }
+                    else if (choices == 4)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_3_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_3_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_3_3;
                     }
                     else
                     {
-                        a = global::dnd.Properties.Resources.Mixed_4;
+                        a = global::dnd.Properties.Resources.Mixed_3_4;
                     }
                     break;
                 case 2:
-                    a = global::dnd.Properties.Resources.Mixed_2;
+                    choices = getRandom(4);
+                    if (choices == 4)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_2_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_2_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_2_3;
+                    }
+                    else
+                    {
+                        a = global::dnd.Properties.Resources.Mixed_2_4;
+                    }
                     break;
                 default:
-                    a = global::dnd.Properties.Resources._1_Metal;
+                    choices = getRandom(4);
+                    if (choices == 4)
+                    {
+                        a = global::dnd.Properties.Resources._1_Metal_1;
+                    }
+                    else if (choices == 3)
+                    {
+                        a = global::dnd.Properties.Resources._1_metal_2;
+                    }
+                    else if (choices == 2)
+                    {
+                        a = global::dnd.Properties.Resources._1_Plastic_3;
+                    }
+                    else
+                    {
+                        a = global::dnd.Properties.Resources._1_Plastic_4;
+                    }
                     break;
             }
             System.Media.SoundPlayer player = new System.Media.SoundPlayer(a);
@@ -219,7 +395,7 @@ namespace dnd
 
         private int getRandom(int v)
         {
-            Random random = new Random();
+            Random random = new Random(Guid.NewGuid().GetHashCode());
             return random.Next(1, v + 1);
         }
 
@@ -231,6 +407,11 @@ namespace dnd
         private void ModDown_Click(object sender, EventArgs e)
         {
             ModNumTxt.Text = "" + (int.Parse(ModNumTxt.Text) - 1);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
