55e16e6a594a9538c12a8c486616d9a9b96b5c73
JayBuckley7 <ghfghf77@gmail.com>
2019-03-21 00:15:42 -0400

    dnd

--- FileRefrenceChecker/Form1.Designer.cs
+++ FileRefrenceChecker/Form1.Designer.cs
@@ -28,20 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.RaveLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // RaveLbl
+            // 
+            this.RaveLbl.AutoSize = true;
+            this.RaveLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.RaveLbl.Location = new System.Drawing.Point(257, 146);
+            this.RaveLbl.Name = "RaveLbl";
+            this.RaveLbl.Size = new System.Drawing.Size(131, 15);
+            this.RaveLbl.TabIndex = 0;
+            this.RaveLbl.Text = "are you here for the rave?";
+            this.RaveLbl.Click += new System.EventHandler(this.RaveLbl_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(587, 311);
+            this.Controls.Add(this.RaveLbl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label RaveLbl;
     }
 }
 
--- FileRefrenceChecker/Form1.cs
+++ FileRefrenceChecker/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
 
 namespace FileRefrenceChecker
 {
@@ -15,6 +17,39 @@ namespace FileRefrenceChecker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void RaveLbl_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                int delay = 10;
+                this.BackColor = SystemColors.ControlDark;
+                this.Refresh();
+                Application.DoEvents();
+                Thread.Sleep(delay);
+                this.BackColor = Color.Red;
+                this.Refresh();
+                Application.DoEvents();
+                Thread.Sleep(delay);
+                this.BackColor = Color.Blue;
+                this.Refresh();
+                Application.DoEvents();
+                Thread.Sleep(delay);
+                this.BackColor = Color.HotPink;
+                this.Refresh();
+                Application.DoEvents();
+                Thread.Sleep(delay);
+                this.BackColor = Color.Yellow;
+                this.Refresh();
+                Application.DoEvents();
+                Thread.Sleep(delay);
+                this.BackColor = Color.Purple;
+                this.Refresh();
+                Application.DoEvents();
+                Thread.Sleep(delay);
+            }
+
         }
     }
 }
